568cf9594733e36ffd0866b4f9fc3e2d0e848a4c
Aloke Desai <aloke.desai@gmail.com>
2014-09-27 01:42:16 -0700

    refactor code; separate into setCurrentSong and setLink methods

--- main.js
+++ main.js
@@ -13,30 +13,27 @@ $(document).ready(function() {
     var buttonString = "&nbsp; <div style='padding-left: 5px' class='miniplayer-info-playlist-title'> <a id='songza'> Open in Spotify </a> </div>";
     if (!($("#songza").length)) {
       $(".miniplayer-info-playlist").append(buttonString);
-
-
-      currentSong["title"] = $(".miniplayer-info-track-title").text();
-      currentSong["artist"] = $(".miniplayer-info-artist-name a").attr("title");
-
-      var albumTitle = $(".miniplayer-info-album-title").text();
-      currentSong["album"] = albumTitle.slice(5, albumTitle.length);
-      console.log(currentSong);
-
-      query = "https://api.spotify.com/v1/search?query=track:" + escape(currentSong["title"]) + "+artist:" + escape(currentSong["artist"]) + "&limit=20&type=track"
-      console.log(query);
-      // make spotify request
-      $.getJSON(query, function(result) {
-        console.log(result);
-        // set the link 
-        if (result["tracks"]["items"].length) {
-          linkURL = result["tracks"]["items"]["0"]["external_urls"]["spotify"];
-          $("#songza").attr("href", linkURL);
-          $("#songza").attr("target", "_blank");
-        }
-      });
+      setCurrentSong();
+      setLink();
     }
   }
 
+  function setCurrentSong() {
+    currentSong["title"] = $(".miniplayer-info-track-title").text();
+    currentSong["artist"] = $(".miniplayer-info-artist-name a").attr("title");
+    albumTitle = $(".miniplayer-info-album-title").text();
+    currentSong["album"] = albumTitle.slice(5, albumTitle.length);
+  }
 
-
+  function setLink() {
+    // make spotify request
+    $.getJSON("https://api.spotify.com/v1/search?query=track:" + escape(currentSong["title"]) + "+artist:" + escape(currentSong["artist"]) + "&limit=20&type=track", function(result) {
+      // set the link 
+      if (result["tracks"]["items"].length) {
+        linkURL = result["tracks"]["items"]["0"]["external_urls"]["spotify"];
+        $("#songza").attr("href", linkURL);
+        $("#songza").attr("target", "_blank");
+      }
+    });
+  }
 });
