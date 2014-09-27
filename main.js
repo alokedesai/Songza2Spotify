012374a5aefd78dd86915d2b54754c51bdbc0950
Aloke Desai <aloke.desai@gmail.com>
2014-09-27 01:36:08 -0700

    successfully link to spotify web app for each song

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var currentSong = {};
-	
+
   var checkExist = setInterval(function() {
    		if ($(".miniplayer-info-track-title").length) {
         // alert("Exists!");
@@ -10,18 +10,29 @@ $(document).ready(function() {
 
 
   function addButton() {
-    var buttonString = "&nbsp; <a class='miniplayer-info-playlist-title' id='songza'> OPEN IN SPOTIFY </a>";
+    var buttonString = "&nbsp; <div style='padding-left: 5px' class='miniplayer-info-playlist-title'> <a id='songza'> Open in Spotify </a> </div>";
     if (!($("#songza").length)) {
       $(".miniplayer-info-playlist").append(buttonString);
+
+
       currentSong["title"] = $(".miniplayer-info-track-title").text();
       currentSong["artist"] = $(".miniplayer-info-artist-name a").attr("title");
 
       var albumTitle = $(".miniplayer-info-album-title").text();
       currentSong["album"] = albumTitle.slice(5, albumTitle.length);
       console.log(currentSong);
-      // add event handler
-      $("#songza").click(function(){
-        console.log(currentSong["title"]);
+
+      query = "https://api.spotify.com/v1/search?query=track:" + escape(currentSong["title"]) + "+artist:" + escape(currentSong["artist"]) + "&limit=20&type=track"
+      console.log(query);
+      // make spotify request
+      $.getJSON(query, function(result) {
+        console.log(result);
+        // set the link 
+        if (result["tracks"]["items"].length) {
+          linkURL = result["tracks"]["items"]["0"]["external_urls"]["spotify"];
+          $("#songza").attr("href", linkURL);
+          $("#songza").attr("target", "_blank");
+        }
       });
     }
   }
