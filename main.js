0cd51e3c28cd7f43877ac7eae95b7a780c95658d
Aloke Desai <aloke.desai@gmail.com>
2014-09-26 22:14:24 -0700

    save current song into currentSong object

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
-	var checkExist = setInterval(function() {
-   		if ($(".miniplayer-info-playlist").length) {
-        alert("Exists!");
+  var currentSong = {};
+	
+  var checkExist = setInterval(function() {
+   		if ($(".miniplayer-info-track-title").length) {
+        // alert("Exists!");
         addButton();
    		}
-	}, 300); // check every 100ms
-});
-var currentSong = {}
+	}, 1000); // check every 100ms
 
-function addButton() {
-  var buttonString = "&nbsp; <div class='miniplayer-info-playlist-title' id='songza' style='color:#3385ff'>ADD TO SPOTIFY </div>";
-  if (!($("#songza").length)) {
-    $(".miniplayer-info-playlist").append(buttonString);
+
+  function addButton() {
+    var buttonString = "&nbsp; <a class='miniplayer-info-playlist-title' id='songza'> OPEN IN SPOTIFY </a>";
+    if (!($("#songza").length)) {
+      $(".miniplayer-info-playlist").append(buttonString);
+      currentSong["title"] = $(".miniplayer-info-track-title").text();
+      currentSong["artist"] = $(".miniplayer-info-artist-name a").attr("title");
+
+      var albumTitle = $(".miniplayer-info-album-title").text();
+      currentSong["album"] = albumTitle.slice(5, albumTitle.length);
+      console.log(currentSong);
+      // add event handler
+      $("#songza").click(function(){
+        console.log(currentSong["title"]);
+      });
+    }
   }
-}
 
+
+
+});
